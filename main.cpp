3a7b256c2a4f6c37003d830db501c49b51752633
qcs4tracy <qcs4tracy@gmail.com>
2015-04-24 12:21:08 -0400

    main

--- main.cpp
+++ main.cpp
@@ -8,14 +8,20 @@ using namespace atm_data;
 
 using namespace zsummer::log4z;
 
-int main () {
+int main (int argc, char *argv[]) {
+
+    if (argc < 2) {
+       cerr << "Usage: ./cnp-atm-server <port>" << endl;
+       exit(255);
+    }
 
     ILog4zManager::getInstance()->start();
     ioloop::IOLoop ioloop(1024);
-    ioloop.setup_listen_sock(INADDR_ANY, 5999);
+    ioloop.setup_listen_sock(INADDR_ANY, ::atoi(argv[1]));
     ioloop.enable_listning(1024);
     ioloop.start();
 
     return 0;
 }
+
 
